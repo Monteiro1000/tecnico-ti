5d3e8732c872fbca88ed321a5a628922d585fc28
Júlio Monteiro <julioalvedmonteiro@gmail.com>
2025-08-25 21:37:59 -0300

    Introdução no assunto de script

---
+++ tipo-de-dados/JavaScript/script.js
@@ -0,0 +1,24 @@
+// 1 - Number
+
+console.log(1234); // integer
+console.log(typeof 1234); // integer
+console.log(12.34); // float
+console.log(typeof 12.34); // float
+console.log(1.2e3); // 1.2 * 10^3
+console.log(typeof 1.2e3); // 1.2 * 10^3
+console.log(-43); // 1.2 * 10^-3
+console.log(typeof -43); // 1.2 * 10^3
+
+// 9 - operador infinity
+
+// 10 -strings
+console.log('isso é uma string'); // string com aspas simples
+console.log("isso é uma string"); // string com aspas duplas
+console.log(`isso é uma string`); // string com aspas crase
+console.log(typeof 'isso é uma string'); // string
+console.log(typeof "isso é uma string"); // string
+console.log(typeof `isso é uma string`); // string
+
+// 11 - Concatenação de Strings
+console.log('Isso é' + 'uma string'); // Concatenação com o operador +
+console.log('Isso é' + 123); // Concatenação com o operador =, número convertido para string
