9fe2c2de0f0dce026b2d02b0bc30f5ba94d24278
Júlio Monteiro <julioalvedmonteiro@gmail.com>
2025-10-20 15:54:37 -0300

    Fazendo alterações no codigo

--- tipo-de-dados/JavaScript/script.js
+++ tipo-de-dados/JavaScript/script.js
@@ -29,12 +29,3 @@ console.log('5' - 5); // 0 - subtração, string convertida para
 console.log('5' * 5); // 25 - multiplicação, string convetida, string convertida
 console.log('10' / 2); // 5 - divisão, string convertida para número
 
-/*Efetuar o cálculo e a apresentação do valor de uma prestação em atraso, utilizando a fórmula PRESTACAO ← VALOR + (VALOR * TAXA/100) * TEMPO).
-*/
-
-var valor = 4000
-var taxa = 1.5
-var tempo = 10
-var prestacao;
-
-prestaçao += (valor * taxa/100) * tempo
---
+++ tipo-de-dados/script.js
@@ -0,0 +1,10 @@
+/*Efetuar o cálculo e a apresentação do valor de uma prestação em atraso, utilizando a fórmula PRESTACAO ← VALOR + (VALOR * TAXA/100) * TEMPO).
+*/
+
+var valor = 4000
+var taxa = 1.5
+var tempo = 10
+var prestacao;
+
+prestaçao += (valor * taxa/100) * tempo
+ 
