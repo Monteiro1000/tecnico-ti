6abf2cc21689a84963ecc6df137a80da2a7250ef
Júlio Monteiro <julioalvedmonteiro@gmail.com>
2025-10-20 21:33:18 -0300

    Testando react e criando um código para testar

--- projet0_node/projeton1/src/App.jsx
+++ projet0_node/projeton1/src/App.jsx
@@ -7,28 +7,19 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+
+      <div oncli>
+        <p className="read-the-docs">Olá, mundo!</p>
+
+        <a href="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS1I813yifkgKQYLVipRyQZESnNvJYooM_YJA&s" target='blank'>Clique em mim!</a>
+        
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 80000)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+      
+      
+        
+      
+    
   )
 }
 
